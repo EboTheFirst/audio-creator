6c546c15f72e8dc1368c546ce02e4eac5760aa1f
Ebo <ebokak5@gmail.com>
2022-09-27 23:26:10 +0100

    Testing audio creation code

--- app/main.py
+++ app/main.py
@@ -4,6 +4,9 @@ from typing import Union
 from fastapi import FastAPI, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+import numpy as np
+from scipy.io.wavfile import write
+
 
 app = FastAPI()
 
@@ -23,8 +26,20 @@ class AudString(BaseModel):
 
 @app.post("/string")
 def test(audio: AudString):
+    dataArray = list(map(float,audio.split(',')))
+    mn = min(dataArray)
+    mx = max(dataArray)
+    prescaled = [((2 *(data - mn))/(mx-mn) - 1) for data in dataArray]
+    scaled = np.int16(prescaled/np.max(np.abs(prescaled)) * 32767)
+
     response = {
-        "msg": "Just returning what I got",
-        "audio": audio
-    }
-    return response
+        "audioArray": scaled,
+        "size": len(scaled),
+        }
+    try:
+        write('test.wav', 2000, scaled)
+    except:
+        response = {
+            "msg": "Something went wrong"
+        }
+    return scaled
